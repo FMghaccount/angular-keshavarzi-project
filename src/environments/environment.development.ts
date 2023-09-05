de53c0b2d9d618e06b6018d8e71f97a5fea991bb
FMghaccount <118979275+FMghaccount@users.noreply.github.com>
2023-09-05 18:58:59 +0330

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  firebaseApiUrl:
-    'https://ng-keshavarzi-default-rtdb.europe-west1.firebasedatabase.app/',
-  firebaseApiKey: 'AIzaSyB2SXgCcBO-uulpMwrX69PXYt9jA9kY75M',
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  firebaseApiUrl:
-    'https://ng-keshavarzi-default-rtdb.europe-west1.firebasedatabase.app/',
-  firebaseApiKey: 'AIzaSyB2SXgCcBO-uulpMwrX69PXYt9jA9kY75M',
-};
